cf9e77bbbcff2716236385d1218d9d16221741cd
Erki Suurjaak <erki@lap.ee>
2024-04-29 21:49:22 +0300

    v1.2.1 checkpoint.

--- src/grepros/__init__.py
+++ src/grepros/__init__.py
@@ -11,8 +11,8 @@ Released under the BSD License.
 """
 ## @namespace grepros
 __title__        = "grepros"
-__version__      = "1.2.1.dev1"
-__version_info__ = (1, 2, 1, "dev1")
+__version__      = "1.2.1"
+__version_info__ = (1, 2, 1)
 __version_date__ = "29.04.2024"
 
 from . library import *
